84de4e247247e7103e9ee24849e9f1c3eeec9b53
Fernanda Domenicali <43413565+nandadomenicali@users.noreply.github.com>
2023-08-11 13:21:24 -0300

    (feat): types bool and string

--- fundamentals/type/types.go
+++ fundamentals/type/types.go
@@ -23,4 +23,17 @@ func main() {
 	var x float32 = 49.99
 	fmt.Println("The type of x is:", reflect.TypeOf(x))
 	fmt.Println("The literal type is:", reflect.TypeOf(49.99))
+
+	bo := true
+	fmt.Println(!bo)
+
+	s1 := "Hello World!"
+	fmt.Println(s1 + "!")
+	fmt.Println("The length of the string is:", len(s1))
+
+	s2 := `Hello
+	my 
+	name
+	is....`
+	fmt.Println("The length of the string is:", len(s2))
 }
